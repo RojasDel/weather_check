4320bb22257260dcac72be580a9954a489facbfa
RojasDel <quechula022@gmail.com>
2024-10-28 09:52:33 -0300

    Complementando con argparse

--- api.py
+++ api.py
@@ -1,23 +1,41 @@
 import requests
 from apikey import api_key
+import requests.exceptions
 
 
-city = "asuncion"
+#city = "asuncion"
 
 def check_weather(city):
 
     url_base = "http://api.weatherapi.com/v1/current.json?"
     params = {"key" : api_key, "q" : city} 
 
-    response = requests.get(url_base, params=params, timeout=5)
-    print(f"response.url")
+    try:
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    
-    else:
-        return f"Err: {response.status_code}, {response.text}"
-    
-weather = check_weather(city)
-print(weather)
+        response = requests.get(url_base, params=params, timeout=5)
+        print(f"{response.url}")
+
+        if response.status_code == 200:
+            data = response.json()
+            location = data["location"]
+            current = data["current"]
+            return {
+                
+                "ciudad": location["name"],
+                "region": location["region"],
+                "pais": location["country"],
+                "temperatura_actual": current["temp_c"],
+                "condicion_actual": current["condition"]["text"],
+                "precipitacion": current["precip_mm"],
+                "humedad": current["humidity"],
+                "fecha_actualizacion": current["last_updated"],
+            }
+        
+        elif response.status_code == 400:
+            raise Exception (f"Ubicacaion no encontrada: {city}")
+        
+        else:
+            raise Exception (f"Err: {response.status_code}")
+    except requests.exceptions.ConnectionError:
+            raise Exception ("Error de connexion")
+            
---
+++ cli_argp.py
@@ -0,0 +1,35 @@
+import argparse
+from api import check_weather
+import json
+import sys
+import csv
+
+def main():
+    parser = argparse.ArgumentParser(description="CLI donde se obtienen datos del clima") #Inicializa parser
+    parser.add_argument("city", help="Indica el nombre de la ciudad")
+    parser.add_argument("format", choices=["json", "csv"], help="Elige el formato de salida json o cvs")
+
+    args=parser.parse_args() #Trae de parser la informacion de args
+
+    city = args.city
+    format = args.format
+
+    print(f"Obtener el clima de {city} en formato {format}.")
+
+    try:
+        weather = check_weather(city)
+        if format == "json":
+            print(json.dumps(weather, indent=2))
+
+        else:
+            #Abrir el archivo en modo escritura
+            #with open(f"{city}weather.csv", mode='w', newline='') as narchivo_csv:
+            writer = csv.DictWriter(sys.stdout, fieldnames=weather.keys())
+            writer.writeheader()
+            writer.writerow(weather)
+
+    except Exception as e:
+        print(f"Err: {e}")
+
+if __name__ == "__main__":
+    main()
